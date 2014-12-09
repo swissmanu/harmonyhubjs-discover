7c8784016bd24fdf9a2af00a9b33e548863bcf05
Manuel Alabor <msites@msites.net>
2014-12-09 23:37:06 +0100

    fixed problem with eventemitter inheritance. emitting proper events when discovered a new hub, also when one disappeared.

--- examples/discover.js
+++ examples/discover.js
@@ -1,4 +1,12 @@
 var Explorer = require('../lib/explorer')
 	, explorer = new Explorer(61991);
 
+explorer.on('online', function(hub) {
+	console.log('discovered ' + hub.ip);
+});
+
+explorer.on('offline', function(hub) {
+	console.log('lost ' + hub.ip);
+});
+
 explorer.start();
