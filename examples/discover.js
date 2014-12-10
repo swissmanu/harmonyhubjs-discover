c676f7aec72cb300810a37b6e7b923f2be24a8f1
Manuel Alabor <msites@msites.net>
2014-12-10 17:18:17 +0100

    introduced update event as suggested by @mweibel

--- examples/discover.js
+++ examples/discover.js
@@ -9,4 +9,12 @@ explorer.on('offline', function(hub) {
 	console.log('lost ' + hub.ip);
 });
 
+explorer.on('update', function(hubs) {
+	var knownHubIps = hubs.reduce(function(prev, hub) {
+			return prev + (prev.length > 0 ? ', ' : '') + hub.ip;
+		}, '');
+
+	console.log('known ips: ' + knownHubIps);
+});
+
 explorer.start();
