89d28d66f52532b709e8f701d84decbfd19fe39b
Manuel Alabor <manuel@alabor.me>
2015-12-28 22:10:56 +0100

    style(standard): Make code standard code style compliant

--- examples/discover.js
+++ examples/discover.js
@@ -1,19 +1,19 @@
-var discover = new (require('../'))(61991);
+var discover = new (require('../'))(61991)
 
-discover.on('online', function(hub) {
-	console.log('discovered ' + hub.ip);
-});
+discover.on('online', function (hub) {
+  console.log('discovered ' + hub.ip)
+})
 
-discover.on('offline', function(hub) {
-	console.log('lost ' + hub.ip);
-});
+discover.on('offline', function (hub) {
+  console.log('lost ' + hub.ip)
+})
 
-discover.on('update', function(hubs) {
-	var knownHubIps = hubs.reduce(function(prev, hub) {
-			return prev + (prev.length > 0 ? ', ' : '') + hub.ip;
-		}, '');
+discover.on('update', function (hubs) {
+  var knownHubIps = hubs.reduce(function (prev, hub) {
+    return prev + (prev.length > 0 ? ', ' : '') + hub.ip
+  }, '')
 
-	console.log('known ips: ' + knownHubIps);
-});
+  console.log('known ips: ' + knownHubIps)
+})
 
-discover.start();
+discover.start()
--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = require('./lib/explorer');
+module.exports = require('./lib/explorer')
